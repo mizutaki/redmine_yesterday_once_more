a965a96fa09cd3b7ba59a38255c20b47b6792a52
mizutaki <mizutaki4@gmail.com>
2016-05-07 21:25:50 +0000

    typo

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_yesterday_once_more do
   author 'mizutaki'
   description 'Add my page blocks what was yesterday'
   version '0.0.1'
-  url 'https://github.com/mizutaki/redmine_yesterday_oncemore'
+  url 'https://github.com/mizutaki/redmine_yesterday_once_more'
   author_url 'http://example.com/about'
 end
