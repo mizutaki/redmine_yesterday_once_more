3577198609bac3144be4f1a126807a2ee1820176
mizutaki <mizutaki4@gmail.com>
2016-05-07 13:34:43 +0000

    To view a sample

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_what_was_yesterday do
+  name 'what was yesterday plugin'
+  author 'mizutaki'
+  description 'Add my page blocks what was yesterday'
+  version '0.0.1'
+  url 'https://github.com/mizutaki/redmine_what_was_yesterday'
+  author_url 'http://example.com/about'
+end
